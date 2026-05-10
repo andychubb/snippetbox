d44db98a30890f725db8341368bf7674633315d7
Andy Chubb <andrew.chubb@outlook.com>
2026-05-10 11:06:28 +0100

    Added in a wildcard segment for the snippetView id

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Defines a home handler
@@ -11,7 +13,17 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet"))
+	// Extract the value of the id wildcard from the request using r.PathValue()
+	// and try to convert it to an integer using the strconv.Atoi() function. if
+	// it can't be converted, or it's value is less than 1, return a 404.
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w,r)
+		return
+	}
+	
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +33,7 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting a server on :4000")
